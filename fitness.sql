54feafe859bc5708d7dce8f3e728faacf2626b94
Timur <93942023+pr1xt@users.noreply.github.com>
2024-06-12 09:57:06 +0200

    database

---
+++ fitness.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Cze 12, 2024 at 09:56 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fitness`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `activity`
+--
+-- Error reading structure for table fitness.activity: #1932 - Table &#039;fitness.activity&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.activity: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`activity`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `admin`
+--
+-- Error reading structure for table fitness.admin: #1932 - Table &#039;fitness.admin&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.admin: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`admin`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `client`
+--
+-- Error reading structure for table fitness.client: #1932 - Table &#039;fitness.client&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.client: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`client`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `clienttimetable`
+--
+-- Error reading structure for table fitness.clienttimetable: #1932 - Table &#039;fitness.clienttimetable&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.clienttimetable: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`clienttimetable`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `instructor`
+--
+-- Error reading structure for table fitness.instructor: #1932 - Table &#039;fitness.instructor&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.instructor: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`instructor`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `instructorguidance`
+--
+-- Error reading structure for table fitness.instructorguidance: #1932 - Table &#039;fitness.instructorguidance&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.instructorguidance: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`instructorguidance`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `membership`
+--
+-- Error reading structure for table fitness.membership: #1932 - Table &#039;fitness.membership&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.membership: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`membership`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pricing`
+--
+-- Error reading structure for table fitness.pricing: #1932 - Table &#039;fitness.pricing&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.pricing: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`pricing`&#039; w linii 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `worker`
+--
+-- Error reading structure for table fitness.worker: #1932 - Table &#039;fitness.worker&#039; doesn&#039;t exist in engine
+-- Error reading data for table fitness.worker: #1064 - Something is wrong in your syntax obok &#039;FROM `fitness`.`worker`&#039; w linii 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
